985d6f7e82edce1e3292d36fc165621e6f15f091
nahyun <developernarong@gmail.com>
2023-11-06 16:28:43 +0900

    css 수정

---
+++ app/api/route.ts
@@ -0,0 +1,71 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface Content {
+  name: string;
+  keyword?: string;
+  desc: string;
+  index: string;
+}
+
+interface DateDetail {
+  title: string;
+  date: string;
+  content: Content[];
+}
+
+interface UserData {
+  year: string;
+  constellation: string;
+}
+
+interface Result {
+  day: DateDetail;
+  tomorrow: DateDetail;
+  month: DateDetail;
+  userData: UserData;
+}
+
+interface JsonResponse {
+  result: Result;
+  resultMSG: string;
+}
+
+export const GET = async (req: NextRequest): Promise<NextResponse> => {
+  const query = req.nextUrl.searchParams;
+  const gender = query.get("gender");
+  const birthdate = query.get("birthdate");
+  const month = query.get("month");
+  const time = query.get("time");
+  console.log(gender, birthdate, month, time);
+  const res = await fetch(
+    `https://m.search.naver.com/p/csearch/dcontent/external_api/json_todayunse_v2.naver?_callback=window.__jindo2_callback._fortune_my_0&gender=${gender}&birth=${birthdate}&solarCal=${month}&time=${time}`
+  );
+  console.log(res);
+
+  function convert(str: string): JsonResponse | null {
+    const fixedStr = str
+      .replace(/^window\.__jindo2_callback\._fortune_my_0\(/, "")
+      .replace(/\);$/, "");
+
+    try {
+      const parsedJson: JsonResponse = JSON.parse(
+        fixedStr.replace(/([\w]+) ?:/g, '"$1":')
+      );
+      if (parsedJson) {
+        return parsedJson;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      return null;
+    }
+  }
+
+  const resTxt = await res.text();
+  const resultData = convert(resTxt);
+  console.log(resultData);
+
+  // https://m.search.naver.com/p/csearch/dcontent/external_api/json_todayunse_v2.naver
+
+  return NextResponse.json(resultData);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+'use client'
+import Image from "next/image"
+import React, { useEffect, useState } from "react"
+import Clover from './../public/images/clover-leaf.png'
+import Clover1 from './../public/images/clover.png'
+import Clover2 from './../public/images/clover (1).png'
+
+
+interface Content {
+  name: string;
+  keyword?: string;
+  desc: string;
+  index: string;
+}
+
 
 export default function Home() {
+
+  const [gender, setGender] = useState<string>("")
+  const [birthDate, setBirthDate] = useState<string>("")
+  const [month, setMonth] = useState<string>("1")
+  const [time, setTime] = useState<string>("")
+
+
+  const [resultToday, setResultToday] = useState(null)
+  const [resultTom, setResultTom] = useState(null)
+  const [resultMonth, setResultMonth] = useState(null)
+  const [result, setResult] = useState<Content[]>([])
+
+
+
+  const fetchData = async ()=>{
+    try {
+      const res = await fetch(`/api?gender=${gender}&birthdate=${birthDate}&month=${month}&time=${time}`);
+      const data = await res.json();
+      
+      if (data.result.day.content.length === 0) {
+        alert("옵션을 선택해주세요.");
+      } else {
+        setResultToday(data.result.day.title);
+        setResultTom(data.result.tomorrow.title);
+        setResultMonth(data.result.month.title);
+        setResult(data.result.day.content);
+      }
+    } catch (error) {
+      console.error("데이터를 가져오는 중 오류가 발생했습니다:", error);
+      alert("데이터를 가져오는 중 오류가 발생했습니다. 나중에 다시 시도해주세요.");
+    }
+  }
+
+  const birthChange = ((e: React.ChangeEvent) =>{
+      const value = e.target.value;
+      if(value.length <= 8 && /^[0-9]*$/.test(value)){
+        setBirthDate(value)
+      }
+
+  })
+
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-full text-center mt-4 pb-8">
+      <div className="mx-auto w-4/5 mt-[100px]">
+
+        <div className="w-[200px] h-[200px] rounded-full border mx-auto animate-rotate">
+          <Image className="mx-auto" width={150} height={150} src={Clover} alt="300x300"/>
+          <h2 className="text-md font-bold">오늘의 운세</h2>
+        
         </div>
+        <div className="flex mt-[40px]  md:border rounded-lg p-5 flex-wrap w-full mx-auto justify-center items-center">
+          <div className="w-full md:w-1/5 mx-auto flex justify-evenly">
+            <button className={`py-2 basis-[48%]  border rounded-xl ${gender === 'm' ? 'bg-green-500' : ''}`} onClick={() => setGender("m")}>남성</button>
+            <button className={`py-2 basis-[48%] border rounded-xl ${gender === 'f' ? 'bg-green-500' : ''}`} onClick={() => setGender("f")}>여성</button>
+          </div>
+          <div className="basis-full md:basis-2/5 flex items-center mt-4">
+            <span className="basis-1/5">생년월일</span>
+            <input className="border py-2 rounded-md basis-4/5" onChange={birthChange} value={birthDate} type="text" placeholder="생년월일 8자리를 입력해주세요"/>
+          </div>
+          <div className="basis-full md:basis-2/5 flex items-center mt-4">
+            <span className="basis-1/5">달</span>
+            <select onChange={(e) => setMonth(e.target.value)} value={month} className="border py-2 rounded-md basis-4/5">
+              <option value="1">양력</option>
+              <option value="2">음력 평달</option>
+              <option value="3">음력 윤달</option>
+            </select>
+          </div>
+          <div className="basis-full md:basis-2/5 flex items-center mt-4">
+            <span className="basis-1/5">시간</span>
+            <select className="border rounded-md py-2 basis-4/5" value={time} onChange={(e) => setTime(e.target.value)}>
+              <option value="">모름</option>
+              <option value="0">子(자) 23:30 ~ 01:29</option>
+              <option value="0">丑(축) 01:30 ~ 03:29</option>
+              <option value="0">寅(인) 03:30 ~ 05:29</option>
+              <option value="0">卯(묘) 05:30 ~ 07:29</option>
+              <option value="0">辰(진) 07:30 ~ 09:29</option>
+              <option value="0">巳(사) 09:30 ~ 11:29</option>
+              <option value="0">未(미) 13:30 ~ 15:29</option>
+              <option value="0">申(신) 15:30 ~ 17:29</option>
+              <option value="0">酉(유) 17:30 ~ 19:29</option>
+              <option value="0">戌(술) 19:30 ~ 21:29</option>
+              <option value="0">亥(해) 21:30 ~ 23:29</option>
+
+            </select>
+          </div>
+          <div className="basis-full md:basis-1/5 md:ml-3 flex items-center mt-4">
+            <button className="border w-full py-2 transition-[0.5s] rounded-xl bg-green-500 hover:bg-slate-700 hover:text-white" onClick={fetchData}>확인</button>
+          </div>
+        </div>
+
+
+          
+      
+            {
+              result && result.map((e,i)=>{
+                return(
+                <div key={i} className="w-4/5 h-auto leading-[45px] p-[20px] mx-auto mt-6 border rounded-md">
+                  <div className="flex flex-wrap" >
+                    <p className="border-b font-bold">{e.name}</p>
+                    <p>{e.desc}</p>
+                  </div>
+                </div>
+                )
+              })
+            }
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
